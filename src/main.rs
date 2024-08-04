1a8db789b894ef2b0b701cf60b2a9b48f8b254c0
Lucas-Haux <hauxlucas2005@gmail.com>
2024-08-04 12:18:56 -0700

    Add number to letter encryption

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use std::io;
+use dialoguer::{theme::ColorfulTheme, Confirm};
 
 fn main() {
     let mut encrepted_message = String::new();
+
+    // Ask the user if they want to encrypt letters to numbers
+    let letter_to_num = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Do you want to encrypt letters to numbers?")
+        .default(false) // Default to "No"
+        .interact()
+        .unwrap();
+
+    if letter_to_num == true {
+        println!("You chose to encrypt letters to numbers.");
+    } else {
+        println!("You chose not to encrypt letters to numbers.");
+    }
 
     //Get the message from user
     println!("Whats the message");
@@ -11,6 +25,14 @@ fn main() {
         .expect("Failed to read input");
     //Remove the line_end symbol at the end of the message
     unencrepted_message = unencrepted_message.trim_end().to_string();
+    // if letter to num is true check if the message has any numbers
+    if letter_to_num == true {
+        for char in unencrepted_message.chars() {
+            if char.is_digit(10) {
+                panic!("Please dont enter a number if you are encrypting letters to numbers");
+            }
+        }
+    }
 
     //Get the shift from user 
     println!("Whats the shift");
@@ -22,7 +44,7 @@ fn main() {
     let shift_num: u8 = match shift.trim().parse() {
         Ok(num) => num,
         Err(_) => {
-            panic!("Please enter a valid integer");
+            panic!("Please enter a number thats less than 255");
         }
     };
   
@@ -48,7 +70,14 @@ fn main() {
             }
         }
         //Shift the character by the shift amount
-        let mut ascii_value = char as u8 + shift_num;
+        let mut ascii_value;
+        if letter_to_num == true {
+            ascii_value = char as u8 - 48 + shift_num;
+            max_size = b'9';
+            size_length = 10;
+        } else {
+            ascii_value = char as u8 + shift_num;
+        }
 
         //If the shifted character goes past the maximum amount it should be
         //Wrap around to the start
