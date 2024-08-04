e1adac57314310091eb164f9d7db12ee5e275ca3
Lucas-Haux <hauxlucas2005@gmail.com>
2024-08-04 10:37:15 -0700

    Remove Debug tests, Remove line_end symbol at the end of the message, Fix encryption if shift > 51

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     io::stdin()
         .read_line(&mut unencrepted_message)
         .expect("Failed to read input");
+    //Remove the line_end symbol at the end of the message
+    unencrepted_message = unencrepted_message.trim_end().to_string();
 
     //Get the shift from user 
     println!("Whats the shift");
@@ -16,13 +18,14 @@ fn main() {
     io::stdin()
         .read_line(&mut shift)
         .expect("Failed to read input");
+    //Convert String to u8 and check if its a number
     let shift_num: u8 = match shift.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             panic!("Please enter a valid integer");
         }
     };
-
+  
     //Encrept the message
     for char in unencrepted_message.chars() {
         //Find if the character is lower/upercase or a number
@@ -32,32 +35,31 @@ fn main() {
             b'a'..=b'z' => {
                 max_size = b'z';
                 size_length = 26;
-                println!("lower");
             },
             b'A'..=b'Z' => {
                 max_size = b'Z';
                 size_length = 26; 
-                println!("upper");
             },
             b'0'..=b'9' => {
                 max_size = b'9';
                 size_length = 10; 
-                println!("number");
             },
             _ => {
-                println!("It's something else");
             }
         }
         //Shift the character by the shift amount
-        let ascii_value = char as u8 + shift_num;
+        let mut ascii_value = char as u8 + shift_num;
+
         //If the shifted character goes past the maximum amount it should be
         //Wrap around to the start
-        let shifted_char = if ascii_value <= max_size {
-            ascii_value as char
-        } else {
-            (ascii_value - size_length) as char
+        loop {
+            if ascii_value > max_size {
+                ascii_value -= size_length
+            } else {
+                break;
+            };
         };
-        encrepted_message.push(shifted_char);
+        encrepted_message.push(ascii_value as char);
     } 
     println!("Encrepted Message: {}", encrepted_message);
 }
